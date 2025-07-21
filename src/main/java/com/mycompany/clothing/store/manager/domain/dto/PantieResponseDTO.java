57a9e2dcb966c61ae99683f456d3ac7daf4cf9f4
moisesRubens <moisesrubens.pro@gmail.com>
2025-07-20 21:05:15 -0300

    feat: add PantieResponseDTO.java record

---
+++ src/main/java/com/mycompany/clothing/store/manager/domain/dto/PantieResponseDTO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
+ */
+package com.mycompany.clothing.store.manager.domain.dto;
+
+/**
+ *
+ * @author moise
+ */
+public record PantieResponseDTO() {
+
+}
