132e5c2aa4a69401e0dbac64e45f349f1e935f32
moisesRubens <moisesrubens.pro@gmail.com>
2025-07-21 11:03:27 -0300

    style: delegate Clothing's style attribute to their specific classes

--- src/main/java/com/mycompany/clothing/store/manager/domain/dto/PantieRequestDTO.java
+++ src/main/java/com/mycompany/clothing/store/manager/domain/dto/PantieRequestDTO.java
@@ -4,10 +4,18 @@
  */
 package com.mycompany.clothing.store.manager.domain.dto;
 
+import com.mycompany.clothing.store.manager.domain.enums.ClothingType;
+import com.mycompany.clothing.store.manager.domain.enums.Gender;
+import com.mycompany.clothing.store.manager.domain.enums.LiningType;
+import com.mycompany.clothing.store.manager.domain.enums.SideType;
+import com.mycompany.clothing.store.manager.domain.enums.WaistType;
+
 /**
  *
  * @author moise
  */
-public record PantieRequestDTO() {
-
+public record PantieRequestDTO(String color, Integer quantity, Double price,
+    String fabric, String brand, String style,
+    Gender gender, String pattern, ClothingType clothingType,
+    Character size, LiningType liningType, WaistType waistType, SideType sideType) implements ClothingRequestDTO {
 }
